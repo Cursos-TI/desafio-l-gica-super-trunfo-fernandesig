30bae00835fb5929348affe6cdf0124f42678165
Igor Fernandes <63010702+fernandesig@users.noreply.github.com>
2025-07-04 03:37:04 +0000

    Nivel Avançado - Switch Case incluído.

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -5,7 +5,7 @@ int main(){
     //variáveis da carta1
     char state[30],username[20], cardcode[5], city[30];
     unsigned long int pop;
-    int poi;
+    int poi, comp;
     float area, gdp,popdensity, gdppercapita;
      
 
@@ -116,19 +116,10 @@ int main(){
     float superpower1 = pop + area + gdp + poi + gdppercapita + (1 / popdensity);
     float superpower2 = pop2 + area2 + gdp2 + poi2 + gdppercapita2 + (1 / popdensity2);
 
-     //Comparacoes
-
-    int compPop = pop > pop2;
-    int compArea = area > area2;
-    int compGdp = gdp > gdp2;
-    int compPoi = poi > poi2;
-    int compDensity = popdensity < popdensity2;
-    int compGdpCapita = gdppercapita > gdppercapita2;
-    int compSuperPower = superpower1 > superpower2;
-
-
 
     printf("Obrigado pelas informações, %s! Aqui temos as cartas cadastradas: \n\n", username);
+
+    //Exibição de cartas
     printf("********************** \n");
     printf("*******CARTA 1******** \n");
     printf("********************** \n");
@@ -141,8 +132,7 @@ int main(){
     printf("PIB: %.2f bilhões de reais \n", gdp);
     printf("Número de Pontos Turísticos: %d \n", poi);
     printf("Densidade Populacional: %.2f hab/km²\n",popdensity);
-    printf("PIB per Capita: %.2f reais \n\n", gdppercapita);
-    
+    printf("PIB per Capita: %.2f reais \n\n", gdppercapita); 
 
 
 
@@ -162,20 +152,92 @@ int main(){
 
    
 
+    //Início de comparações
 
-    printf("***************************** \n");
-    printf("COMPARAÇÃO CARTAS (POPULAÇÃO) \n");
-    printf("***************************** \n");    
+    printf("****************************** \n");
+    printf("****COMPARAÇÃO DE ATRIBUTO**** \n");
+    printf("****************************** \n");    
 
-    printf("Carta 1 - %s, %s: %d \n", city, state, pop);
-    printf("Carta 2 - %s, %s: %d \n", city2, state2, pop2);
+    printf("Qual atributo você gostaria de comparar?\n");
+    printf("1. População\n");
+    printf("2. Área\n");
+    printf("3. PIB\n");
+    printf("4. Número de Pontos Turísticos\n");
+    printf("5. Densidade Populacional\n");
+    printf("Qual atributo você gostaria de comparar?\n");
 
-    if (pop > pop2){
-        printf("Resultado: Carta 1 - (%s) venceu! \n", city);
-    } else {
-        printf("Resultado: Carta 2 - (%s) venceu! \n", city2);
-    }
+    scanf("%d", &comp);
+    
+    printf ("Cidade da carta 1: %s\n", city);
+    printf ("Cidade da carta 2: %s\n", city2);
+    printf ("O atributo usado para comparar foi ");
+    
 
+  switch (comp) {
+    case 1:
+        printf("população.\n");
+        printf("População da Carta 1: %lu\n", pop);
+        printf("População da Carta 2: %lu\n", pop2);
+        if (pop > pop2)
+            printf("A cidade vencedora é: %s - Carta 1\n", city);
+        else if (pop < pop2)
+            printf("A cidade vencedora é: %s - Carta 2\n", city2);
+        else
+            printf("Temos um empate.\n");
+        break;
+
+    case 2:
+        printf("área.\n");
+        printf("Área da Carta 1: %.2f\n", area);
+        printf("Área da Carta 2: %.2f\n", area2);
+        if (area > area2)
+            printf("A cidade vencedora é: %s - Carta 1\n", city);
+        else if (area < area2)
+            printf("A cidade vencedora é: %s - Carta 2\n", city2);
+        else
+            printf("Temos um empate.\n");
+        break;
+
+    case 3:
+        printf("PIB.\n");
+        printf("PIB da Carta 1: %.2f\n", gdp);
+        printf("PIB da Carta 2: %.2f\n", gdp2);
+        if (gdp > gdp2)
+            printf("A cidade vencedora é: %s - Carta 1\n", city);
+        else if (gdp < gdp2)
+            printf("A cidade vencedora é: %s - Carta 2\n", city2);
+        else
+            printf("Temos um empate.\n");
+        break;
+
+    case 4:
+        printf("número de pontos turísticos.\n");
+        printf("Pontos Turísticos da Carta 1: %d\n", poi);
+        printf("Pontos Turísticos da Carta 2: %d\n", poi2);
+        if (poi > poi2)
+            printf("A cidade vencedora é: %s - Carta 1\n", city);
+        else if (poi < poi2)
+            printf("A cidade vencedora é: %s - Carta 2\n", city2);
+        else
+            printf("Temos um empate.\n");
+        break;
+
+    case 5:
+        printf("densidade populacional.\n");
+        printf("Densidade Populacional da Carta 1: %.2f\n", popdensity);
+        printf("Densidade Populacional da Carta 2: %.2f\n", popdensity2);
+        if (popdensity < popdensity2)
+            printf("A cidade vencedora é: %s - Carta 1\n", city);
+        else if (popdensity > popdensity2)
+            printf("A cidade vencedora é: %s - Carta 2\n", city2);
+        else
+            printf("Temos um empate.\n");
+        break;
+
+    default:
+        printf("opção inválida.\n");
+        break;
+}
 
     return 0;
 
